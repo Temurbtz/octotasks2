2fc0934d7956c5653c968d16eac5c6064d4a2937
Temurbekjonbtz <56353734+Temurbekjonbtz@users.noreply.github.com>
2023-02-26 11:15:16 +0500

    Initial commit for main branch

---
+++ src/main/java/com/tasks/octotasks2/Octotasks2Application.java
@@ -0,0 +1,13 @@
+package com.tasks.octotasks2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Octotasks2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Octotasks2Application.class, args);
+	}
+
+}
